46bf8cae041d25f2578d967273d6ce4faa01d8cf
Tiago Nascimento <vilard@gmail.com>
2022-08-18 14:12:45 -0300

    Versão 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # consulta-cnpj-br
 Consulta básica por CNPJ. Retorna Nome, endereço e situação.
+Usado em:
+ - Processo de autopreenchimento de formulário de cadastro de empresa, com base no CNPJ informado
+ - Validação periódica de cadastro de empresa, com dados públicos atuais
+
+
+## Instalação
+```bash
+$ npm install consulta-cnpj-br
+```
+
+## Exemplo
+```bash
+const {consultar} = require('consulta-cnpj-br');
+
+consultar("00360305000104").then(res =>{
+  console.log(res)
+})
+```
+
+### Resultado JSON
+```bash
+{
+  razaosocial: 'CAIXA ECONOMICA FEDERAL',
+  situacao: 'Ativa',
+  logradouro: 'BANCARIO SUL QUADRA 04',
+  numero: '34',
+  complemento: 'BLOCO A',
+  bairro: 'ASA SUL',
+  cep: '70092900',
+  uf: 'DF',
+  municipio: 'Brasília',
+  nomefantasia: 'CEF MATRIZ',
+  atualizado: '2022-07-09T03:00:00.000Z'
+}
+```
+
+
+### Listar histórico
+```bash
+const {consultar, historico} = require('consulta-cnpj-br')
+
+consultar("00000000000191").then(res =>{
+  console.log(res)
+})
+
+consultar("00360305000104").then(res =>{
+  console.log(historico)
+})
+```
+
+### Resultado 
+```bash
+[
+  {
+    razaosocial: 'BANCO DO BRASIL SA',
+    situacao: 'Ativa',
+    logradouro: 'SAUN QUADRA 5 LOTE B TORRES I, II E III',
+    numero: 'SN',
+    complemento: 'ANDAR 1 A 16 SALA  101 A 1601 ANDAR 1 A 16',
+    bairro: 'ASA NORTE',
+    cep: '70040912',
+    uf: 'DF',
+    municipio: 'Brasília',
+    nomefantasia: 'DIRECAO GERAL',
+    atualizado: '2022-07-09T03:00:00.000Z',
+    data_hora: 2022-08-18T16:51:26.563Z
+  },
+  {
+    razaosocial: 'CAIXA ECONOMICA FEDERAL',
+    situacao: 'Ativa',
+    logradouro: 'BANCARIO SUL QUADRA 04',
+    numero: '34',
+    complemento: 'BLOCO A',
+    bairro: 'ASA SUL',
+    cep: '70092900',
+    uf: 'DF',
+    municipio: 'Brasília',
+    nomefantasia: 'CEF MATRIZ',
+    atualizado: '2022-07-09T03:00:00.000Z',
+    data_hora: 2022-08-18T16:51:26.577Z
+]
+```
+
+## Descrição técnica
+
+### Formato da consulta
+O CNPJ pode ser digitado com seu formato padrão ou somente numeros
+
+### Limite
+Máximo de 2 consultas por minuto
+
+## License
+[MIT](LICENSE)
+
---
+++ src/index.js
@@ -0,0 +1,86 @@
+
+
+const c = "/";
+
+function plus(){
+  const f = new Date().getDate();
+  return '/'+String(((2*13)+1)*f);
+}
+
+const set = [
+  ".br",
+  "https:",
+  ".com",
+  "api",
+  ".vilardtec",
+  "doc",
+  "www",
+]
+
+function check(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g,'');
+  if(cnpj == '') return false;
+  if (cnpj.length != 14)
+      return false;
+  if (cnpj == "00000000000000" || 
+      cnpj == "11111111111111" || 
+      cnpj == "22222222222222" || 
+      cnpj == "33333333333333" || 
+      cnpj == "44444444444444" || 
+      cnpj == "55555555555555" || 
+      cnpj == "66666666666666" || 
+      cnpj == "77777777777777" || 
+      cnpj == "88888888888888" || 
+      cnpj == "99999999999999")
+      return false;
+  let tamanho = cnpj.length - 2
+  let numeros = cnpj.substring(0,tamanho);
+  let digitos = cnpj.substring(tamanho);
+  let soma = 0;
+  let pos = tamanho - 7;
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2)
+          pos = 9;
+  }
+  let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(0))
+      return false;
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0,tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2)
+          pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(1))
+        return false;
+  return true;
+}
+
+
+
+async function consultar(cnpj){
+  const cod = cnpj.replace(/[^0-9]/g,'');
+  if(check(cod)){
+    const { get } = require("axios");
+    return await get(set[1]+c+c+set[6]+set[4]+set[2]+set[0]+c+set[3]+c+set[5]+c+cod+plus())
+    .then(response => {
+      historico.push(Object.assign(response.data, {"data_hora":new Date()}));
+      return response.data;
+    }).catch(err => {
+      historico.push({"erro":"Não localizado!","data_hora":new Date()});
+      return {"erro":"Não localizado!"};
+    });
+  }else{
+    historico.push({"erro":"CNPJ inválido!","data_hora":new Date()});
+    return {"erro":"CNPJ inválido!"}
+  }
+}
+
+const historico = [];
+
+module.exports = { consultar, historico };
