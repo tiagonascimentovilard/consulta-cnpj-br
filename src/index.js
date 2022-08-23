0b49c8f0709c3ff6c8cae6668e67d6f8dda32c1a
Tiago Nascimento <vilard@gmail.com>
2022-08-23 08:47:47 -0300

    Versão 1.0.1

--- src/index.js
+++ src/index.js
@@ -1,38 +1,27 @@
 
-
-const c = "/";
-
 function plus(){
   const f = new Date().getDate();
   return '/'+String(((2*13)+1)*f);
 }
 
-const set = [
-  ".br",
-  "https:",
-  ".com",
-  "api",
-  ".vilardtec",
-  "doc",
-  "www",
-]
+const set = "https://www.vilardtec.com.br/api/doc/";
 
 function check(cnpj) {
-  cnpj = cnpj.replace(/[^\d]+/g,'');
+  cnpj = cnpj.replace(/[^0-9]/g,'');
   if(cnpj == '') return false;
   if (cnpj.length != 14)
-      return false;
+    return false;
   if (cnpj == "00000000000000" || 
-      cnpj == "11111111111111" || 
-      cnpj == "22222222222222" || 
-      cnpj == "33333333333333" || 
-      cnpj == "44444444444444" || 
-      cnpj == "55555555555555" || 
-      cnpj == "66666666666666" || 
-      cnpj == "77777777777777" || 
-      cnpj == "88888888888888" || 
-      cnpj == "99999999999999")
-      return false;
+    cnpj == "11111111111111" || 
+    cnpj == "22222222222222" || 
+    cnpj == "33333333333333" || 
+    cnpj == "44444444444444" || 
+    cnpj == "55555555555555" || 
+    cnpj == "66666666666666" || 
+    cnpj == "77777777777777" || 
+    cnpj == "88888888888888" || 
+    cnpj == "99999999999999")
+    return false;
   let tamanho = cnpj.length - 2
   let numeros = cnpj.substring(0,tamanho);
   let digitos = cnpj.substring(tamanho);
@@ -41,11 +30,11 @@ function check(cnpj) {
   for (let i = tamanho; i >= 1; i--) {
     soma += numeros.charAt(tamanho - i) * pos--;
     if (pos < 2)
-          pos = 9;
+      pos = 9;
   }
   let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
   if (resultado != digitos.charAt(0))
-      return false;
+    return false;
   tamanho = tamanho + 1;
   numeros = cnpj.substring(0,tamanho);
   soma = 0;
@@ -57,17 +46,15 @@ function check(cnpj) {
   }
   resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
   if (resultado != digitos.charAt(1))
-        return false;
+    return false;
   return true;
 }
 
-
-
 async function consultar(cnpj){
-  const cod = cnpj.replace(/[^0-9]/g,'');
-  if(check(cod)){
+  if(check(cnpj)){
+    const cod = cnpj.replace(/[^0-9]/g,'');
     const { get } = require("axios");
-    return await get(set[1]+c+c+set[6]+set[4]+set[2]+set[0]+c+set[3]+c+set[5]+c+cod+plus())
+    return await get(set + cod + plus())
     .then(response => {
       historico.push(Object.assign(response.data, {"data_hora":new Date()}));
       return response.data;
